d1bd64db0566a72ad1bc7e6a5220b99a02c19b42
Natalia <107213946+nataliahood@users.noreply.github.com>
2022-07-16 14:09:17 +0300

    Delete cypress directory

--- cypress/integration/1-My-tests/google_tt_setch.js
+++
@@ -1,8 +0,0 @@
-
-describe('Тестирование отображения блока TicTok в результатах поска google.com', function () {
-   it('Поиск по "котята" выводит блок видео с TicTok', function () {
-        cy.visit('https://www.google.com/');
-        cy.get('input.gLFyf').type('котята').type('{enter}');
-        cy.contains('Видео с хэштегом #котята');
-    })
-})
--- cypress/integration/1-My-tests/hh_serch_qa.js
+++
@@ -1,9 +0,0 @@
-
-describe('Тестирование поиска вакансий на hh.ru', function () {
-
-    it('Поиск по тексту "qa" выводит вакансии для тестировщиков', function () {
-        cy.visit('https://kazan.hh.ru/');
-        cy.get('.bloko-input').type('qa').type('{enter}');
-        cy.contains('Тестировщик')
-    })
-})
